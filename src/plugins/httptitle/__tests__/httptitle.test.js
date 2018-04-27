7dc9bc95181fd23b370beefcb141762b869481b1
Joni Hiltunen <djonih@gmail.com>
2018-04-27 20:03:40 +0300

    Httptitle test cleanup

--- src/plugins/httptitle/__tests__/httptitle.test.js
+++ src/plugins/httptitle/__tests__/httptitle.test.js
@@ -2,44 +2,61 @@
 
 const ht = require('../httptitle');
 
-function testMsg(msg: string, correct: Array<string>, done: () => void): void {
+type TestCase = {
+  description: string,
+  msg: string,
+  correct: Array<string>,
+}
+
+function testMsg({ msg, correct }: TestCase, done: () => void): void {
   ht.handle(msg).then((x) => {
     expect(x).toEqual(correct);
     done();
   });
 }
 
-test('It works for youtube titles', (done) => {
-  const msg = 'asdasd https://www.youtube.com/watch?v=P73REgj-3UE https://youtu.be/XKs5mkEpWuU https://www.youtube.com/watch?v=GOJUNJ1o394';
-  const correct = [
-    'YT (Primitive Technology): Primitive Technology: Tiled Roof Hut',
-    'YT (juckis): Wings - PELIMUSA - Jussi Huhtala (game music)',
-    'YT (alt-J): alt-J - Deadcrush (Official Video)',
-  ];
+function testOne(testCase: TestCase): void {
+  const { description } = testCase;
+  test(description, (done) => {
+    testMsg(testCase, done);
+  });
+}
 
-  testMsg(msg, correct, done);
-});
+function testAll(tests: Array<TestCase>): void {
+  tests.forEach(testOne);
+}
 
-test('It returns undefined with no urls', (done) => {
-  const msg = 'no urls here';
-  const correct = [];
+const tests : Array<TestCase> = [
+  {
+    description: 'It works for youtube titles',
+    msg: 'asdasd https://www.youtube.com/watch?v=P73REgj-3UE https://youtu.be/XKs5mkEpWuU https://www.youtube.com/watch?v=GOJUNJ1o394',
+    correct: [
+      'YT (Primitive Technology): Primitive Technology: Tiled Roof Hut',
+      'YT (juckis): Wings - PELIMUSA - Jussi Huhtala (game music)',
+      'YT (alt-J): alt-J - Deadcrush (Official Video)',
+    ],
+  },
 
-  testMsg(msg, correct, done);
-});
+  {
+    description: 'It returns undefined with no urls',
+    msg: 'no urls here',
+    correct: [],
+  },
 
-test('It works for random urls', (done) => {
-  const msg = 'http://google.fi yee http://www.yle.fi';
-  const correct = [
-    'title: Google',
-    'title: Yle.fi - oivalla jotain uutta',
-  ];
+  {
+    description: 'It works for random urls',
+    msg: 'http://google.fi yee http://www.yle.fi',
+    correct: [
+      'title: Google',
+      'title: Yle.fi - oivalla jotain uutta',
+    ],
+  },
 
-  testMsg(msg, correct, done);
-});
+  {
+    description: 'ISO-8859-1 works',
+    msg: 'http://www.iltalehti.fi/ulkomaat/201804272200907446_ul.shtml',
+    correct: ['title: Merkel tapaa tänään Trumpin: Saksalaismedia ihmettelee lyhyttä tapaamista'],
+  },
+];
 
-test('Iltalehti works xD', (done) => {
-  const msg = 'http://www.iltalehti.fi/ulkomaat/201804272200907446_ul.shtml';
-  const correct = ['title: Merkel tapaa tänään Trumpin: Saksalaismedia ihmettelee lyhyttä tapaamista'];
-
-  testMsg(msg, correct, done);
-});
+testAll(tests);
